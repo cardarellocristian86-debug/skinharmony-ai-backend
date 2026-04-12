35b56bfb4d3ef1a0df73d8d7cc5320e6f978f77f
Cristian Cardarello <tua-email-github@example.com>
2026-04-12 19:31:58 +0200

    Fix protocol photo analysis submit

--- smartdesk-live/server.js
+++ smartdesk-live/server.js
@@ -217,7 +217,7 @@ app.use((req, res, next) => {
   return next();
 });
 
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json({ limit: "15mb" }));
 app.use("/assets", express.static(path.join(publicDir, "assets")));
 app.use("/exports", express.static(path.join(publicDir, "exports")));
 
